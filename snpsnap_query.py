f9fff3910c1124e4ace3b5282bad71c9d904760a
Pascal Timshel OSX <pascal.timshel@gmail.com>
2014-05-02 19:12:01 -0400

    work finished 05/02/2014 - snpsnp_query.py close to running.

--- snpsnap_query.py
+++ snpsnap_query.py
@@ -42,7 +42,8 @@ def locate_HDF5_data(path, prefix):
 	#TODO fix this. Make checks
 	file_db = "{path}/{type}_db.{ext}".format(path=path, type=prefix, ext='h5')
 	file_meta = "{path}/{type}_meta.{ext}".format(path=path, type=prefix, ext='h5')
-	if not ( os.path.exists(file_db) and os.path.exists(file_meta) ): # both file must exists
+	#if not ( os.path.exists(file_db) and os.path.exists(file_meta) ): # both file must exists
+	if not os.path.exists(file_db): # TODO- FIX THIS LATER
 		print "Could not find database files. Exiting"
 		sys.exit(1)
 	return (file_db, file_meta)
@@ -100,28 +101,78 @@ def write_user_snps_stats(path_output, user_snps, df):
 
 def query_similar_snps(file_db, path_output, df, N_sample_sets, max_freq_deviation, max_distance_deviation, max_genes_count_deviation):
 	user_snps_matrix_file = path_output+"/matrix.out"
+	np.random.seed(1)
+	n_attempts = 5
+
 	store = pd.HDFStore(file_db, 'r')
 	for i in xrange(len(df.index)): # pandas DF indecies is zero based like python
-		pfreq = df.ix[i,'freq_bin']
-		pdensity = df.ix[i,'gene_count']
-		pdist = df.ix[i,'dist_nearest_gene']
+		query_snpID = df.index[i]
+		freq = df.ix[i,'freq_bin']
+		gene_count = df.ix[i,'gene_count']
+		dist = df.ix[i,'dist_nearest_gene']
 
-		query_prim_freq = '(freq_bin = %s)' % pfreq
-		query_prim_density = '(gene_count = %s)' % pdensity
-		query_prim_dist = '(dist_nearest_gene = %s)' % pdist
+		### Setting delta space ####
+		delta_freq = np.rint(np.linspace(0,max_freq_deviation, n_attempts)).astype(int) # rounds to nearest integer and convert to int
+		# OBS: delta_gene_count and delta_dist are in the range of [-1;1]
+		delta_gene_count = np.linspace(0,max_genes_count_deviation, n_attempts)/float(100)
+		if max_distance_deviation < 1:
+		    print "Warning: max_distance_deviation set to %s. Lowest possible max_distance_deviation is 1." % max_distance_deviation
+		    max_distance_deviation = 1
+		delta_dist = np.linspace(1,max_distance_deviation, n_attempts)/float(100) # OBS distance deviation starts at 1 %
+
+		### Calculating low/high boundaries
+		freq_low = np.repeat(freq, n_attempts) - delta_freq # ABSOLUTE DEVIATION
+		freq_high = np.repeat(freq, n_attempts) + delta_freq # ABSOLUTE DEVIATION
+		gene_count_low = np.repeat(gene_count, n_attempts)*(1-delta_gene_count)
+		gene_count_high = np.repeat(gene_count, n_attempts)*(1+delta_gene_count)
+		dist_low = np.repeat(dist, n_attempts)*(1-delta_dist)
+		dist_high = np.repeat(dist, n_attempts)*(1+delta_dist)
+
+		#print (freq_low, freq_high)
+		#print (gene_count_low, gene_count_high)
+		#print (dist_low, dist_high)
+
+		match_ID_old = None
+		match_ID = None
+		for i in xrange(n_attempts):
+		    query_freq = '(freq_bin >= %s & freq_bin <= %s)' % (freq_low[i], freq_high[i])
+		    query_gene_count = '(gene_count >= %s & gene_count <= %s)' % (gene_count_low[i], gene_count_high[i])
+		    query_dist = '(dist_nearest_gene  >= %s & dist_nearest_gene  <= %s)' % (dist_low[i], dist_high[i])
+		    
+		    query = "%s & %s & %s" % (query_freq, query_gene_count, query_dist)
+		    match_ID = store.select('dummy', query, columns=[]).index.values # return no columns --> only index
+		    #df_matches = store.select('dummy', query)
+		    
+		    print "SNP: {%s} query #%d: found %d hits" % ("placeholder", i, len(match_ID))
+		    if len(match_ID) < N_sample_sets:
+		        match_ID_old = match_ID
+		    else: #we have enough matches
+		        break
+
+		#df_matches.head(20)
+		#print len(match_ID_old)
+		#print len(match_ID)
+
+
+		if len(match_ID) < N_sample_sets:
+			print "******** Found SNP with too few matches; n_matches=%s" % len(match_ID)
+		else:
+			match_ID_uniq_new = np.setdiff1d(match_ID, match_ID_old, assume_unique=True) #Return the sorted, unique values in ar1 that are not in ar2
+			#print len(match_ID_uniq_new)# validated: gives same result as print len(match_ID) - len(match_ID_old)
+			n_elements_to_fill = N_sample_sets - len(match_ID_old)
+			#print n_elements_to_fill
+			match_ID_uniq_new_sample = np.random.choice(match_ID_uniq_new, size=n_elements_to_fill, replace=False, p=None) # sample uniformly from NEW matches
+			match_ID_final = np.concatenate((match_ID_old, match_ID_uniq_new_sample))
+			#print len(match_ID_final)
+
+			with open(user_snps_matrix_file,'a') as f_handle:
+			    np.savetxt(f_handle, np.insert(match_ID_final, 0, query_snpID), fmt="%s", newline="\t")
 	
-		query_prim = "%s & %s & %s" % (query_prim_freq, query_prim_density, query_prim_dist)
-		df_retrive = store.select('dummy', query_prim, columns=[]) # return no columns --> only index	
-
-
-	query_margin_freq = '(freq_bin >= %s & freq_bin <= %s)' % (1, 30)
-	query_margin_density = '(gene_count >= %s & gene_count <= %s)' % (2,5)
-	query_margin_dist = '(dist_nearest_gene  >= %s & dist_nearest_gene  <= %s)' % (450, 500)
-
-	query_margin = "%s & %s & %s" % (query_marging_dist, query_marging_freq, query_marging_density)
-	selB=store.select('dummy', query_margin)	
 
 	store.close()
+
+
+
 
 
 #TODO: check input variable types!
@@ -142,7 +193,8 @@ args = arg_parser.parse_args()
 
 
 ### CONSTANTS ###
-path_data = "/Users/pascaltimshel/snpsnap/data/step3/tmp" ## HARD CODED PATH!!
+#path_data = "/Users/pascaltimshel/snpsnap/data/step3/tmp" ## HARD CODED PATH!!
+path_data = "/Users/pascaltimshel/snpsnap/data/step3" ## HARD CODED PATH!!
 path_data = os.path.abspath(path_data) # make sure that trailing newline is removed. DELETE LATER!
 prefix = args.distance_type + args.distance_cutoff
 
@@ -153,7 +205,7 @@ user_snps = read_user_snps(args.user_snps_file) # Read input SNPs. Return dict
 user_snps_df = lookup_user_snps(file_db, user_snps) # Query DB, return DF
 write_user_snps_stats(path_output, user_snps, user_snps_df) # Report matches to DB and write stats file
 print user_snps_df
-query_similar_snps(path_output, user_snps_df)
+query_similar_snps(file_db, path_output, user_snps_df, args.N_sample_sets, args.max_freq_deviation, args.max_distance_deviation, args.max_genes_count_deviation)
 
 
 #matched_snp_sets,matched_snp_nearestgene,matched_snp_genes = get_matched_snps(observed_snps,random_snps,args.max_freq_deviation, allowed_min_frq, allowed_max_frq, args.output_snp_nr,args.max_distance_deviation, args.max_genes_count_deviation)
