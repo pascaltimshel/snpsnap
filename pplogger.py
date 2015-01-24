1fdfb28c2ed6623a84cd27d6d201f9a9e96f2a5a
Pascal Timshel Broad <pascal.timshel@gmail.com>
2015-01-24 00:04:41 -0500

    added some nice changes to pplogger

--- pplogger.py
+++ pplogger.py
@@ -14,20 +14,39 @@ import pphelper
 class Logger(object):
 	""" The intended use of this class is the following call:
 	current_script_name = os.path.basename(__file__).replace('.py','')
-	logger = Logger(name=current_script_name, log_dir=outputdir, log_format=1, enabled=True).get() """
-	def __init__(self, name='NoLogNameParsed', log_dir=os.getcwd(), log_format=1, enabled=True):
+	logger = Logger(name=current_script_name, log_dir=outputdir, log_format=1, enabled=True).get() 
+
+	### Arguments ###
+	log2stream
+		default is True.
+		Determines if the logging should be send to the stream. That is, print the messages.
+		Python's logging module [.StreamHandler()] uses *sys.stderr* by default.
+		Note that "enabled=False" will overwrite this setting and disable ALL logging
+		Example call - will only log to screen:
+			logger = pplogger.Logger(enabled=True, log2stream=True, log2file=False).get()
+
+	log2file
+		default is True.
+		Determines if the logging should be written to a file.
+		Note that "enabled=False" will overwrite this setting and disable ALL logging
+		Example call - will only log to file:
+			logger = pplogger.Logger(enabled=True, log2stream=False, log2file=True).get()
+
+	"""
+	def __init__(self, name='NoLogNameParsed', log_dir=os.getcwd(), log_format=1, enabled=True, log2stream=True, log2file=True):
 		self.name = "logger.%s" % name
 		self.log_dir = log_dir
 		self.log_format = log_format
 		self.enabled = enabled
+		self.log2stream = log2stream
+		self.log2file = log2file
 
 		if self.enabled:
 			self.logger = self.setup_logger()
+			# Call class method to log an 'instantiation of the log'. NB: nothing will happen (be logged) if enable=False
+			self.log_mark_of_initiation()
 		else: 
 			self.logger = self.setup_null_handler()
-
-		# Call class method to log an 'instantiation of the log'. NB: nothing will happen (be logged) if enable=False
-		self.log_mark_of_initiation()
 
 		#TODO: implement a optional log_note. Like
 		#logger.info( "INSTANTIATION NOTE: placeholder" )
@@ -42,23 +61,8 @@ class Logger(object):
 		# Set logging level - this can be changed/overwritten later by the module importing Logger()
 		logger.setLevel(logging.DEBUG) #specifies the lowest-severity log message a logger will handle
 
-		################## Setting log file name ##################
-		# ## USE THIS to create a log file name with a TIME STAMP (READ: file_log is not the same as logger)
-		# ## However, since the logging handlers (file handle, formatting etc.) are only setup ONCE for the class several instantiations of the class will NOT create different log files (file_log)
-		# ## This will effectively mean that each call to a given python script (start of interpreter) will have its OWN log file (because the time stamp will never be the same)
-		# ## Remember that ultimately it is the NAME in "logging.getLogger( "myroot.%s" % name )" that determines what logger (and thus filehandle) to use for the class instance.
-		# file_log = '{name}_{time}.{ext}'.format( name=name, time=pphelper.HelperUtils.gen_timestamp(), ext='log' )
-		
-		## USE THIS to create ONE log file that is appended to.
-		## The file_log is appended to as long as the Logger() class is called with the same 'name'
-		self.file_log = '{name}.{ext}'.format( name=self.name, ext='log' )
-		#########################################
-
 		## only run all this setup if no previous logger has been configured
 		if not logger.handlers:
-			self.file_name = os.path.join(self.log_dir, self.file_log) # OBS setting instance variable
-
-			fh = logging.FileHandler(self.file_name)
 
 			if self.log_format == 0:
 				fh_formatter = logging.Formatter('%(message)s')
@@ -87,18 +91,42 @@ class Logger(object):
 				fh_formatter = logging.Formatter('%(asctime)s	%(levelname)s	%(message)s', '%Y-%m-%d %H:%M:%S')
 				ch_formatter = logging.Formatter('%(message)s')
 
+			if self.log2file:
+				################## Setting log file name ##################
+				# ##### USE THIS to create a log file name with a TIME STAMP (READ: file_log is not the same as logger) #####
+				# ## However, since the logging handlers (file handle, formatting etc.) are only setup ONCE for the class several instantiations of the class will NOT create different log files (file_log)
+				# ## This will effectively mean that each call to a given python script (start of interpreter) will have its OWN log file (because the time stamp will never be the same)
+				# ## Remember that ultimately it is the NAME in "logging.getLogger( "myroot.%s" % name )" that determines what logger (and thus filehandle) to use for the class instance.
+				# file_log = '{name}_{time}.{ext}'.format( name=name, time=pphelper.HelperUtils.gen_timestamp(), ext='log' )
+				
+				##### USE THIS to create ONE log file that is appended to #####
+				## The file_log is appended to as long as the Logger() class is called with the same 'name'
+				self.file_log = '{name}.{ext}'.format( name=self.name, ext='log' )
+				#########################################
 
-			fh.setFormatter(fh_formatter)
-			fh.setLevel(logging.DEBUG)
+				self.file_name = os.path.join(self.log_dir, self.file_log) # OBS setting instance variable
+				fh = logging.FileHandler(self.file_name)
+				fh.setFormatter(fh_formatter)
+				fh.setLevel(logging.DEBUG)
+				logger.addHandler(fh) # Add handler to the logger
 
-			ch = logging.StreamHandler()
-			#ch = logging.StreamHandler() # default 
-			ch.setLevel(logging.INFO)
-			ch.setFormatter(ch_formatter)
-			# Add handler to the logger
-			logger.addHandler(fh)
-			logger.addHandler(ch)
+			if self.log2stream:
+				ch = logging.StreamHandler()
+				ch.setLevel(logging.INFO)
+				ch.setFormatter(ch_formatter)
+				logger.addHandler(ch) # Add handler to the logger
 		
+		### Notes for .addHandler()
+		# The addHandler() method has no minimum or maximum quota for the number of handlers you may add.
+		
+		### About the root logger ###
+		# Source: https://docs.python.org/2/library/logging.html
+		# As seen from the snippet of documentation below, the root logger is configured by called ".basicConfig" using default options.
+		# This means that the roots handler will be .StreamHandler(), which prints to sys.stderr
+		# logging.basicConfig([**kwargs])
+		# 	Does basic configuration for the logging system by creating a StreamHandler with a default Formatter and adding it to the root logger. The functions debug(), info(), warning(), error() and critical() will call basicConfig() automatically if no handlers are defined for the root logger.
+		# 	This function does nothing if the root logger already has handlers configured for it.
+
 		return logger
 
 	def setup_null_handler(self):
