2ab7103f43dbfc8ce67cf19472668aafe1ffdfdc
Pascal Timshel Broad <pascal.timshel@gmail.com>
2015-02-25 22:32:16 -0500

    ADDED scripts for parse_matched_SNPs on Broad.

---
+++ parse_matched_SNPs_broad.py
@@ -0,0 +1,315 @@
+#! /usr/bin/env python2.7
+
+import glob
+import pdb
+import sys
+import collections
+import os
+import argparse
+import re #Pascal
+
+import time
+
+import pplaunch
+import pphelper
+import pplogger
+
+
+def makehash():
+	return collections.defaultdict(makehash) 
+
+# Function to read gene positions
+def read_gene_info(infile):
+	print "Reading ENSEMBL file into hashes"
+	# Ensembl Gene ID,Transcript Start (bp),Transcript End (bp),Strand,Gene Start (bp),Chromosome Name,Gene End (bp)
+	# ENSG00000239156,20113,20230,1,20113,GL000228.1,20230
+	# ENSG00000238762,22673,22791,1,22673,GL000228.1,22791
+	# ENSG00000240442,21191674,21191827,1,21191674,18,21191827
+	infile = open(infile,"r")
+	lines = infile.readlines()[1:] #REMEMBER: skip head line
+	info = makehash()
+	info_red = makehash()
+	chr_pattern = re.compile('^([1-9]|1[0-9]|2[0-4]|[X,Y])$', re.IGNORECASE) # this should match the numeric range 1-24 and X,Y. Match case insensitive.
+	for line in lines:
+		words = line.strip().split(',') # comma separated file
+		chr = words[5]
+
+		# IMPORTANT: there exists strange chr names such as GL000228.1, LRG_13 etc...
+		if not chr_pattern.match(chr):
+			continue 
+
+		### ************Outcommented on Broad 2015************
+		# # Convert everything to numeric to enable comparison afterwards
+		# if chr == "X":
+		# 	chr = "23"
+		# if chr == "Y":
+		# 	chr = "24"
+
+		# Saving ENSEMBLE ID in dict
+		info[chr][words[0]] = 1
+
+		########################## USING TRANSCRIPTION START/END ######################
+		### OBS: that there may be many different Transcript Start/End FOR THE SAME ENSEMBL ID
+		### ----> SEE 'exp_gene_definition.txt' for details
+
+		
+		#if words[0] == 'ENSG00000176771': pdb.set_trace()
+		########################## USING GENE START/END ######################
+		if words[3] == '1': # Strand is FORWARD
+			info_red[words[0]]['sta'] = int(words[4]) # ===> Gene Start (bp)
+			info_red[words[0]]['end'] = int(words[6]) # ===> Gene End (bp)
+		elif words[3] == '-1': # Strand is REVERSE - then START should be END by ENSEMBL convention
+			info_red[words[0]]['sta'] = int(words[6]) # ===> Gene End (bp)
+			info_red[words[0]]['end'] = int(words[4]) # ===> Gene Start (bp)
+		else:
+			print "Warning: strange 'Strand' column entry in ENSEMBL file. Expected '1' or '-1' got [%s]. The below shows the full line:\n%s" % ( words[3], line )
+			#TODO: make some exeption or print
+			# Currently we skip genes that do have stran '1' or '-1'
+			# This is OK since I checked this: 
+			#	cut -d',' -f4 ensg_mart_ensembl64_buildGRCh37.p5.tab | egrep -v '(-1)|(1)'
+			# 	--> gives only "Strand"
+
+	infile.close()
+	print "done reading ENSEMBL file"
+
+	return info,info_red
+
+# Function that reads ld files and prints matches SNPs
+def get_matched_snps(path,outfilename):
+	#TODO: gene_info,gene_info_red are NOT parsed as arguments to this function.
+	#	- instead they are global variables. BAD PRACTICE!!!! 
+	# gene_info == info
+	# gene_info_red == info_red
+	outfile = open(outfilename,'w')
+	# outfile = open(outfilename,'w', buffering=0) # JUNE 18 2014 - only for TEST - do not use without buffer
+	ldfiles = glob.glob(path+"*.ld")
+	for ldfile in ldfiles:
+		print "Running ldfile={}".format(ldfile)
+
+		matched_snps_boundaries = makehash()
+	
+		# Setting freq_bin
+		# example:  ...long_path/freq1-2-part0-10000.ld
+		filename_ld = os.path.basename(ldfile) # gives e.g. freq1-2-part0-10000.ld
+		freq_bin = '??'
+		if 'freq' in filename_ld:
+			tmp1 = filename_ld.split('freq')[1] # gives e.g. 1-2-part0-10000.ld
+			freq_bin = tmp1.split('-')[0] # gives e.g. 1
+		else:
+			print "Warning: unknow ldfile name format. Cannot set freq_bin. Check source code"
+
+
+		### Count the number of lines in the file
+		n_lines_ldfile = sum(1 for line in open(ldfile,'r')) # this is pretty fast (only a bit slower than "wc -l"). 3.5 sec for a 1 GB file
+
+		time_start_loop = time.time()
+
+		print "Readings all SNPs and finding their LD budies (matched_snps_boundaries)"
+		# Read all SNPs and their LD budies
+		with open(ldfile, 'r') as infile:
+			next(infile) # SKIPPING THE FIRST LINE!
+			for line_no, line in enumerate(infile, start=1):
+				
+				### Some stats
+				if line_no % 10000 == 0:
+					time_elapsed_loop = time.time() - time_start_loop # <type 'float'>
+					print "Main loop | #{line_no}/#{n_lines} | {pct_complete:.2f} % done | {sec:.2f} sec [{min:.2f} min]".format(line_no=line_no, n_lines=n_lines_ldfile, pct_complete=(line_no/float(n_lines_ldfile))*100, sec=time_elapsed_loop, min=time_elapsed_loop/60)
+				
+				### Split and process
+				words = line.strip().split()
+				
+				if not words[2] in matched_snps_boundaries:
+					# words[2] == input SNP?
+					# CHR_A         BP_A        SNP_A  CHR_B         BP_B        SNP_B           R2
+	     			# 1      1011095   rs11810785      1      1011095   rs11810785            1
+	     			# 1      1011095   rs11810785      1      1025301    rs9442400      0.61996
+					matched_snps_boundaries[words[2]]['chr'] = int(words[0])
+					matched_snps_boundaries[words[2]]['pos'] = int(words[1])
+					matched_snps_boundaries[words[2]]['up'] = int(words[4])
+					matched_snps_boundaries[words[2]]['down'] = int(words[4])
+
+					############## NEW JUNE 18 2014 ###############
+					matched_snps_boundaries[words[2]]['ld_buddies'] = 0 # first time we see the SNP the buddy is with itself
+				else:
+					matched_snps_boundaries[words[2]]['ld_buddies'] += 1 # we count the number of ld_buddies
+					if int(words[4]) < matched_snps_boundaries[words[2]]['up']:
+						matched_snps_boundaries[words[2]]['up'] = int(words[4])
+					if int(words[4]) > matched_snps_boundaries[words[2]]['down']:
+						matched_snps_boundaries[words[2]]['down'] = int(words[4])
+			
+		
+		# Loop over matched SNPs and report gene density as observed SNP
+		#for matched_rsID in matched_snps_boundaries: # <<<<--- THIS unsorted loop was used for generating 1KG_snpsnap_production_v1 data on CBS (the parse_matched_SNPs.py) was run around June 20 (06/20/2014).
+		# ^^ It is impractical printing out the SNPs in 'random' order for each bin. If you sort on the matched_rsID (rsID) you should get SNP files with rsID in the same order. Then you can use 'cut' and other unix tools
+		# NB: the sorting should be ok. The freq1-2 bin have around 1 Million SNPs, which needs to be sorted
+		# OBS: consider using collections.OrderedDict() which remembers the order in which the elements have been inserted
+		#	----> no do not do that. Sorting is more stable because we do not have to reply on the order in which the SNPs is inserted.
+		print "sorting keys of matched_snps_boundaries"
+		matched_snps_boundaries_sorted_keys = sorted(matched_snps_boundaries.keys())
+		print "DONE sorting keys of matched_snps_boundaries"
+
+
+		count_total = len(matched_snps_boundaries_sorted_keys)
+		time_start_loop = time.time()
+
+		for count_now, matched_rsID in enumerate(matched_snps_boundaries_sorted_keys, start=1): #
+
+			### Some stats
+			if count_now % 1000 == 0:
+				time_elapsed_loop = time.time() - time_start_loop # <type 'float'>
+				print "Main loop | #{count_now}/#{count_total} | {pct_complete:.2f} % done | {sec:.2f} sec [{min:.2f} min]".format(count_now=count_now, count_total=count_total, pct_complete=(count_now/float(count_total))*100, sec=time_elapsed_loop, min=time_elapsed_loop/60)
+			
+
+			### processing
+			snp_chr = matched_snps_boundaries[matched_rsID]['chr'] # NB. integer!
+			snp_position = matched_snps_boundaries[matched_rsID]['pos'] # NB. integer!
+
+			genes_in_matched_locus = {} # orig
+		
+			matched_nearest_gene = "" 
+			matched_nearest_gene_dist = float("inf")
+			
+			### NEW JUNE 18 2014 ########
+			matched_nearest_gene_located_within = "" 
+			matched_nearest_gene_dist_located_within = float("inf")
+
+			# Loop over all genes on chromosome
+			# TODO: sort the genes. Running the loop in a sorted order gives MORE RELIABLE results (at least when debugging etc.)
+			#	---> Pascal likes sorted dicts...
+			# OBS: consider using collections.OrderedDict() which remembers the order in which the elements have been inserted
+			#	----> Yes use this. Do not sort this has inside another for loop. That will be waay to slow.
+			for gene in gene_info[str(snp_chr)]: # e.g. gene_info['5'] returns all ENSEMBL IDs in chromosome 5.
+
+				# gene is a key in hash, i.e. a ENSEMBL ID string. gene_info[] is a hash
+				#gene_start is transcription start site
+				gene_start = gene_info_red[gene]['sta'] # This should be an integer
+
+				gene_end = gene_info_red[gene]['end'] # This should be an integer
+		
+				# Mark if gene overlaps matched locus
+				##@DOC We cover four possible scenarios: 
+				# 1. line: gene START is within locus. this applies both to forward and reverse genes
+				# 2. line: gene END is within locus. this applies both to forward and reverse genes
+				# 3. line: gene extends through locus. gene is on FORWARD strand (gene_start < gene_end)
+				# 4. line: gene extends through locus. gene is on REVERSE strand (gene_start > gene_end)
+				# NB: read_gene_info() sets the gene gene_start and gene_end correctly according the the ENSEMBL convention "strand" information
+				if (int(gene_start) > matched_snps_boundaries[matched_rsID]['up'] and int(gene_start) < matched_snps_boundaries[matched_rsID]['down']) \
+				or (int(gene_end) > matched_snps_boundaries[matched_rsID]['up'] and int(gene_end) < matched_snps_boundaries[matched_rsID]['down']) \
+				or (int(gene_start) < matched_snps_boundaries[matched_rsID]['up'] and int(gene_end) > matched_snps_boundaries[matched_rsID]['down']) \
+				or (int(gene_end) < matched_snps_boundaries[matched_rsID]['up'] and int(gene_start) > matched_snps_boundaries[matched_rsID]['down']):
+					genes_in_matched_locus[gene]= 1
+				
+		
+				########### FINDING NEAREST GENE AND DIST #########
+				# Update nearest gene
+				dist = abs( int(gene_start) - snp_position ) #
+				if dist < matched_nearest_gene_dist: 
+					matched_nearest_gene = gene 
+					matched_nearest_gene_dist = dist # this is dist_nearest_gene
+
+
+				################ NEW JUNE 18 2014 - FINDING NEAREST GENE DIST WITHIN #############
+				## 1. gene_start <= snp_position <= gene_end | ===> SNP position within gene on FORWARD strand
+				## 2. gene_end <= snp_position <= gene_start | ===> SNP position within gene on REVERSE strand
+				# NB: you could "merge" the nested 'if statement' by using "and". 
+				# I chose a nested if statement for code readability
+				if (gene_start <= snp_position <= gene_end) \
+				or (gene_end <= snp_position <= gene_start): # if condition is true ===> SNP position is inside gene
+					if dist < matched_nearest_gene_dist_located_within: # if condition in true ===> distance to current gene is the smallest seen so far
+						matched_nearest_gene_located_within = gene
+						matched_nearest_gene_dist_located_within = dist
+
+
+			# Report gene density
+			matched_gene_count = len(genes_in_matched_locus)
+			
+
+			################ NEW JUNE 18 2014 - WRITE OUT #############
+
+			#1=rsID
+			#2=freq_bin
+			#3=chromosome number of rsID
+			#4=position of rsID
+			#5=gene count in matched locus
+			#6=dist to nearest gene - SNPSNAP DISTANCE (NEW!)
+			#7=dist to nearest gene
+			#8=dist to nearest gene LOCATED WITHIN (NEW!)
+			#9=boundary_upstream
+			#10=boundary_downstream
+			#11=nearest_gene SNPSNAP (NEW!)
+			#12=nearest_gene
+			#13=nearest_gene LOCATED WITHIN (may be empty) (NEW!)
+			#14=ld buddies count (NEW!)
+			#15=genes in matches locus, multiple ENSEMBL IDs
+
+			####### NOW FIND the "snpsnap" distance and ENSEMBL gene:
+			## "snpsnap" gene distance: 
+			## 1) distance to gene where SNP is located within the gene has highest precedence. (matched_nearest_gene_dist_located_within)
+			## 2) If the SNP is not located within a gene, then use the distance to the nearest gene (matched_nearest_gene_dist)
+			
+			## Initialize snpsnap distance variables
+			matched_nearest_gene_snpsnap = ""
+			matched_nearest_gene_dist_snpsnap = float("inf") # this is just one way to initialize the variable. You could also use e.g None
+
+			if matched_nearest_gene_dist_located_within != float("inf"): # if matched_nearest_gene_dist_located_within is NOT float('inf') then the SNP is located within a gene
+				matched_nearest_gene_snpsnap = matched_nearest_gene_located_within
+				matched_nearest_gene_dist_snpsnap = matched_nearest_gene_dist_located_within
+			else: # SNP is not located within a gene
+				matched_nearest_gene_snpsnap = matched_nearest_gene
+				matched_nearest_gene_dist_snpsnap = matched_nearest_gene_dist
+
+
+			#TODO: 
+			# - consider printing 'NA' instead of nothing if genes_in_matched_locus is empty.
+			# - this will give a nicer human readble output
+			# - both R (na.strings = "NA") and pandas will handle this as a missing value by default 
+
+			# IMPORTANT: 
+			# - REMEMBER THAT THE NUMBER OF COLUMS MUST BE UPDATED IN THE VALIDATION IN run_parse_matched_SNPs.py
+			# - First column MUST be rs_ID
+			outfile.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}\t{11}\t{12}\t{13}\t{14}\n".format( \
+				matched_rsID, \
+				freq_bin, \
+				snp_chr, \
+				snp_position, \
+				matched_gene_count, \
+				matched_nearest_gene_dist_snpsnap, \
+				matched_nearest_gene_dist, \
+				matched_nearest_gene_dist_located_within, \
+				matched_snps_boundaries[matched_rsID]['up'], \
+				matched_snps_boundaries[matched_rsID]['down'], \
+				matched_nearest_gene_snpsnap, \
+				matched_nearest_gene, \
+				matched_nearest_gene_located_within, \
+				matched_snps_boundaries[matched_rsID]['ld_buddies'], \
+			";".join(genes_in_matched_locus.keys()) )) # pascal - with added extension
+				
+
+
+
+	outfile.close()
+	
+################################# ENSEMBL Gene file #######################################################
+#gene_information_file="/home/projects/tp/childrens/snpsnap/data/misc/ensg_mart_ensembl64_buildGRCh37.p5.tab"
+gene_information_file="/cvar/jhlab/snpsnap/data/misc/ensg_mart_ensembl64_buildGRCh37.p5.tab"
+# Ensembl Gene ID,Transcript Start (bp),Transcript End (bp),Strand,Gene Start (bp),Chromosome Name,Gene End (bp)
+# ENSG00000239156,20113,20230,1,20113,GL000228.1,20230
+# ENSG00000238762,22673,22791,1,22673,GL000228.1,22791
+# ENSG00000240442,21191674,21191827,1,21191674,18,21191827
+# ENSG00000242521,21868882,21868958,1,21868882,18,21868958
+# ENSG00000244478,63142749,63142836,1,63142749,18,63142836
+#
+# Parse arguments  
+#
+arg_parser = argparse.ArgumentParser(description="Parse plink output and construct loci")
+arg_parser.add_argument("--ldfiles_prefix", help="Prefix to Plink .ld files ('*' suffixed in above method)")
+arg_parser.add_argument("--outfilename", help="Filename for outfile") # e.g e.g. .;long_path../ld0.5/stat_gene_density/freq0-1.tab
+args = arg_parser.parse_args()
+
+#
+# Analysis
+#
+gene_info,gene_info_red = read_gene_info(gene_information_file) # Finds position of each gene
+# saves hashes global
+
+get_matched_snps(args.ldfiles_prefix,args.outfilename)
---
+++ run_parse_matched_SNPs_broad.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python2.7
+#@TODO Possibly change this
+
+## USAGE: this script calls parse_matched_SNPs.py.
+## parse_matched_SNPs.py is call with a specific freq bin to process, e.g. the bin freq0-1
+## EXAMPLE call:
+##
+## BEWARE: the PATH of parse_matched_SNPs.py is hardcoded.
+
+
+import sys
+import glob
+import os
+import datetime
+import time
+import subprocess 
+#sys.path.append('/home/projects/tp/tools/matched_snps/src') # Outcommented Broad 2015-02
+
+#from queue import QueueJob,ShellUtils,ArgparseAdditionalUtils #ArgparseAdditionalUtils unused?
+import pdb
+import argparse
+
+import collections
+
+
+import pplaunch
+import pphelper
+import pplogger
+
+
+############################ SETTING ZERO BUFFERING for STDOUT ######################################
+# Consider this for unbuffered output. This may be useful when the function is called via run_multiple_...py
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'wb', 0)
+batch_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H.%M.%S')
+#######################################################################
+
+
+
+def run_parse(snplist_prefix, outfilename, unit_test_file):
+	snplist_files = glob.glob(snplist_prefix+"*.rsID") # catching all files with CORRECT prefix
+	if not os.path.exists(outfilename): # Test if already run
+		unit_test_file['NO_PREVIOUS_FILE'].append(outfilename)
+		#print "%s: CREATING NEW" % outfilename # OUTCOMMENTED JUNE 18 2014
+		
+		return True
+	else:
+		batch_snplist = {}
+		existing_outfile = {}			
+		# Read existing outfile. .tab is a 6 column file. First column is rs-number for matched_rsID
+		#rs16823904      9-10    3       154081865       7       11940   ENSG00000240048 ENSG00000240068,ENSG00000174953,ENSG00000174948
+		with open(outfilename, 'r') as f:
+			lines = f.readlines() # No header...
+			#pdb.set_trace()
+			#expected_cols = 10 ######################## OBS ##############################
+			#expected_cols = 13 ######################## OBS - NEW JUNE 18 2014 - after adding 2 x located within (dist and ID) + 1 x LD buddies ##############################
+			expected_cols = 15 ######################## OBS - NEW JUNE 19 2014 - after adding x2 SNPsnap distance (dist and ID) ##############################
+			for line in lines:
+				# Remove only trailing newline
+				cols = line.rstrip('\n').split('\t') # tab seperated - WE MUST KNOW THIS!
+				# cols[0] ==> "input SNP rs-number" (matched_rsID)
+				if len(cols) == expected_cols: #hmmm, potentially bad code
+					# if cols[0] in existing_outfile: pdb.set_trace()
+					existing_outfile[cols[0]] = 1
+				else:
+					logger.critical( "***OBS*** File %s did not contain %d columns as expected." % (outfilename, expected_cols) )
+					logger.critical( "Please check structure of file if you see the message repeatedly" )
+					break
+		#pdb.set_trace()
+		for snp_list in snplist_files:
+			with open(snp_list, 'r') as f:
+				# rs28615451
+				# rs184229306
+				# rs115111187
+				# rs12361890
+				lines = f.readlines()
+				for line in lines:
+					rs_no = line.strip()
+					batch_snplist[rs_no] = 1
+		#pdb.set_trace()
+		if len(batch_snplist) == len(existing_outfile):
+			unit_test_file['FILE_EXISTS_OK'].append(outfilename)
+			#print "%s: OK" % outfilename # OUTCOMMENTED JUNE 18 2014
+
+			return None # Do not submit new job if files are ok!
+		else:
+			unit_test_file['FILE_EXISTS_BAD'].append(outfilename)
+			#print "%s: BAD FILE EXISTS. MAKING NEW" % outfilename # OUTCOMMENTED JUNE 18 2014
+
+			return True # Re-run job.
+
+
+
+
+# Submit
+def submit(path, stat_gene_density_path):
+	#prefix = os.path.abspath(path+"/ldlists/freq")
+	block_str = '============================= %s ===================================' % batch_time
+	#unit_test_file = {}
+	unit_test_file = collections.defaultdict(list)
+	# Initialyzing keys
+	unit_test_file['NO_PREVIOUS_FILE']
+	unit_test_file['FILE_EXISTS_OK']
+	unit_test_file['FILE_EXISTS_BAD']
+
+	processes = []
+	for i in range(0,50,1): # 0...49. allways 50 freq bins. No problem here.
+		suffix = "%s-%s"%(i,i+1) # e.g. 0-1
+		#outfilename = prefix + suffix + ".tab" # e.g. ...long-path.../ldlists/freq0-1.tab
+		ldfiles_prefix = os.path.abspath(path+"/ldlists/freq") + suffix
+		snplist_prefix = os.path.abspath(path+"/snplists/freq") + suffix # e.g. # e.g. ...long-path.../snplist/freq0-1, where the whole file is e.g. freq9-10-part40000-50000.rsID
+		outfilename = stat_gene_density_path + "/freq" + suffix + ".tab" # e.g. .;long_path../step2/1KG_full_queue/ld0.5/stat_gene_density/freq0-1.tab
+		
+		run = run_parse(snplist_prefix, outfilename, unit_test_file)
+		
+		#command = "%s --ldfiles_prefix %s --outfilename %s"%(script,"%s%s"%(prefix,suffix),outfilename) # e.g. --ldfiles_prefix becomes freq0-1
+		command = "python {script2call} --ldfiles_prefix {ldfiles_prefix} --outfilename {outfilename}".format(script2call=script2call, ldfiles_prefix=ldfiles_prefix, outfilename=outfilename) #
+				
+		jobname = super_population + "_" + distance_measure + "_run_parse_matched_SNPs_" + suffix # e.g. ??
+
+		if run:
+			logger.info( "will submit job for ldfiles_prefix: %s" % ldfiles_prefix )
+			processes.append( pplaunch.LaunchBsub(cmd=command, queue_name=queue_name, mem=mem, jobname=jobname, projectname='snpsnap', path_stdout=log_dir, file_output=None, no_output=False, email=email, email_status_notification=email_status_notification, email_report=email_report, logger=logger) ) #
+			#processes.append( QueueJob(command, log_dir_path, queue_name, walltime, mem_per_job , flags, "run_parse_matched_SNPs_"+suffix, script_name=current_script_name) )
+		else: 
+			logger.info( "will NOT submit job for ldfiles_prefix: %s" % ldfiles_prefix )
+
+		# if not os.path.exists(outfilename):
+		# 		processes.append( QueueJob(command, log_dir_path, queue_name, walltime, mem_per_job , flags, "run_parse_matched_SNPs_"+suffix, script_name=current_script_name) )
+		# elif sum(1 for line in open(outfilename)) == 0: # Files are emtpy
+		# 		processes.append( QueueJob(command, log_dir_path, queue_name, walltime, mem_per_job , flags, "run_parse_matched_SNPs_"+suffix, script_name=current_script_name) )
+	
+	################## Display STATS ##########################
+	logger.info( '\n'.join([block_str]*3) )
+	logger.info( "#################### **** STATS from 'unit_test_file' **** ####################" )
+	for stat_key, stat_list in unit_test_file.items():
+		logger.info( "{}: {}".format( stat_key, len(stat_list) ) )
+	logger.info( block_str )
+	for stat_key, stat_list in unit_test_file.items():
+		#logger.info( "{}: {}".format( stat_key, len(stat_list) ) )
+		for ldfile in stat_list:
+			logger.info( "{}\t{}".format( stat_key, ldfile ) )
+		logger.info( block_str )
+
+	################## NOW SUBMIT JOBS #######################
+	for p in processes:
+		p.run()
+
+	return processes
+	# ################## PRINT FAILS ##########################
+	# print '\n'.join([block_str]*3)
+	# print "#################### **** JOBS THAT COULD NOT BE SUBMITTED - from QueueJob **** ####################"
+	# print "Number of jobs that were not submitted: %s" % len(QueueJob.QJ_job_fails_list)
+	# for no, job_name in enumerate(QueueJob.QJ_job_fails_list, start=1):
+	# 	print "{}\t{}".format(no, job_name)
+	# print '\n'.join([block_str]*3)
+
+
+###################################### ... ######################################
+
+
+def check_jobs(processes, logger):
+	logger.info("PRINTING IDs")
+	list_of_pids = []
+	for p in processes:
+		logger.info(p.id)
+		list_of_pids.append(p.id)
+
+	logger.info( " ".join(list_of_pids) )
+
+	# if args.multiprocess:
+	# 	logger.info( "Running report_status_multiprocess " )
+	# 	pplaunch.LaunchBsub.report_status_multiprocess(list_of_pids, logger) # MULTIPROCESS
+	# else:
+	logger.info( "Running report_status" )
+	pplaunch.LaunchBsub.report_status(list_of_pids, logger) # NO MULTIPROCESS
+
+
+def ParseArguments():
+	# Parse arguments  
+	arg_parser = argparse.ArgumentParser(description="Run parse_matched_SNPs.py")
+	arg_parser.add_argument("--plink_matched_snps_path", \
+		help="""Path for plink_matched_snps.py results which contain 3 directories [ldlists, log, snplists],
+	e.g. /home/projects/tp/childrens/snpsnap/data/step2/1KG_full_queue/ld0.5/
+	NB. please use symlinks in the path, i.e. do not use /net/home...""", required=True)
+	#NB. Also check that the path has a trailing slash (/)
+	
+	args = arg_parser.parse_args()
+	return args
+
+def LogArguments():
+	# PRINT RUNNING DESCRIPTION 
+	now = datetime.datetime.now()
+	logger.critical( '# ' + ' '.join(sys.argv) )
+	logger.critical( '# ' + now.strftime("%a %b %d %Y %H:%M") )
+	logger.critical( '# CWD: ' + os.getcwd() )
+	logger.critical( '# COMMAND LINE PARAMETERS SET TO:' )
+	for arg in dir(args):
+		if arg[:1]!='_':
+			logger.critical( '# \t' + "{:<30}".format(arg) + "{:<30}".format(getattr(args, arg)) ) ## LOGGING
+
+
+
+###################################### Global params ######################################
+#queue_name = "hour" # [bhour, bweek] priority
+#queue_name = "priority" # [bhour, bweek] priority
+queue_name = "week" # [bhour, bweek] priority
+# priority: This queue has a per-user limit of 10 running jobs, and a run time limit of three days.
+mem="20" # gb      
+	### RESULTS from EUR_chr_1 (largest chromosome)
+email='pascal.timshel@gmail.com' # [use an email address 'pascal.timshel@gmail.com' or 'False'/'None']
+email_status_notification=True # [True or False]
+email_report=False # # [True or False]
+
+current_script_name = os.path.basename(__file__).replace('.py','')
+
+###################################### ARGUMENTS ######################################
+args = ParseArguments()
+
+###################################### .... ######################################
+################## script2call ##################
+script2call = "/cvar/jhlab/snpsnap/snpsnap/parse_matched_SNPs_broad.py"
+
+
+
+path = os.path.abspath(args.plink_matched_snps_path) # Trailing slash are removed/corrected - NICE!
+#^^^ E.g. "/home/projects/tp/tools/matched_snps/src/hapmap/ld0.5/"
+#^^^ E.g. "/home/projects/tp/childrens/snpsnap/data/step2/1KG_test_thin0.1/ld0.5/"
+log_dir_path = path + "/log" #PASCAL: PROBLEM fixed with extra slash
+stat_gene_density_path = path + "/stat_gene_density"
+if not os.path.exists(stat_gene_density_path):
+	os.makedirs(stat_gene_density_path)
+
+################## OBS: *HAAACK* Getting "UNIQUE ID" from "--plink_matched_snps_path" ##################
+## Example
+#os.path.basename(os.path.dirname("/cvar/jhlab/snpsnap/data/step2/1KG_snpsnap_production_v2/EUR/ld0.5"))
+# -->'ld0.5'
+distance_measure = os.path.basename(os.path.abspath(path)) # e.g. ld0.5
+super_population = os.path.basename(os.path.dirname(os.path.abspath(path))) # e.g. EUR
+
+
+# The "pipeline_identifier" should contain information to form a UNIQUE identifier. This IDENTIFIER IS ONLY CONVENIENT FOR LOGGING/DEBUGGING purposes.
+	# Relevant information needed --> super_population, distance_type and 
+
+###################################### .... ######################################
+
+
+
+###################################### SETUP logging ######################################
+log_dir = "/cvar/jhlab/snpsnap/logs_pipeline/production_v2/step2_run_parse_matched_SNPs_broad/{super_population}".format(super_population=super_population) #OBS
+if not os.path.exists(log_dir):
+	os.makedirs(log_dir)
+log_name = current_script_name + "_{super_population}_{distance_measure}".format(super_population=super_population, distance_measure=distance_measure) #OBS
+
+logger = pplogger.Logger(name=log_name, log_dir=log_dir, log_format=1, enabled=True).get()
+def handleException(excType, excValue, traceback, logger=logger):
+	logger.error("Logging an uncaught exception", exc_info=(excType, excValue, traceback))
+#### TURN THIS ON OR OFF: must correspond to enabled='True'/'False'
+sys.excepthook = handleException
+logger.info( "INSTANTIATION NOTE: placeholder" )
+###########################################################################################
+
+
+
+
+
+###################################### RUN FUNCTIONS ######################################
+# NOW RUN FUNCTIONS
+LogArguments()
+
+### Submit jobs
+processes = submit(path, stat_gene_density_path)
+
+start_time_check_jobs = time.time()
+# OBS: DO NOT WAIT FOR JOBS
+#check_jobs(processes, logger) # TODO: parse multiprocess argument?
+elapsed_time = time.time() - start_time_check_jobs
+logger.info( "Total Runtime for check_jobs: %s s (%s min)" % (elapsed_time, elapsed_time/60) )
+logger.critical( "%s: finished" % current_script_name)
+
+
+
+
+
